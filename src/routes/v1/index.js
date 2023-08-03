a9d10dc5c9b70d6d81dd837c19ab7ca27374cb3d
ashmitpatel1402 <ashmit14patel@gmail.com>
2023-08-03 14:09:34 +0530

    Bug fixes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const CityControllers=require('../../controllers/city-controllers');
 const router=express.Router();
 
 router.post('/city',CityControllers.create);
+router.delete('/city/:id',CityControllers.destroy);
+router.get('/city:id',CityControllers.get);
+router.patch('/city/:id',CityControllers.update);
 
 module.exports=router;
