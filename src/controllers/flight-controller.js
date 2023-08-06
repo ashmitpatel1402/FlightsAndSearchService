89274070fc3413a10c6efa265362ed46458c50bb
ashmitpatel1402 <ashmit14patel@gmail.com>
2023-08-06 11:52:41 +0530

    Added flight create middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -3,8 +3,17 @@ const {FlightService}=require('../services/index');
 const flightService=new FlightService();
 
 const create=async (req,res)=>{
-    try{
-         const flight=await flightService.createFlight(req.body);
+    try{ let flightRequestData={//done to avoid passing unwanted data as it makes our object bulky
+        flightNumber:req.body.flightNumber,
+        airplaneId:req.body.airplaneId,
+        departureAirportId:req.body.departureAirportId,
+        arrivalAirportId:req.body.arrivalAirportId,
+        arrivalTime:req.body.arrivalTime,
+        departureTime:req.body.departureTime,
+        price:req.body.price
+    };
+          
+         const flight=await flightService.createFlight(flightRequestData);
          return res.status.json({
             data:flight,
             success:true,
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const CityControllers=require('../../controllers/city-controllers');
 const FlightController=require('../../controllers/flight-controller');
 const AirportController=require('../../controllers/airport-controller');
+const {FlightMiddlewares}=require('../../middlewares/index');
 const router=express.Router();
 
 router.post('/city',CityControllers.create);
@@ -10,9 +11,11 @@ router.get('/city:id',CityControllers.get);
 router.patch('/city/:id',CityControllers.update);
 router.get('/city',CityControllers.getAll);
 
-router.post('/flights',FlightController.create);
+router.post('/flights',
+            FlightMiddlewares.checkCreateFlight,
+            FlightController.create
+);           
 router.get('/flights',FlightController.getAll);
-
 router.post('./airports',AirportController.create);
 router.patch('./airports/:id',AirportController.update);
 
